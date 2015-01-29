44f08b326ffed161f793c7e2a3c66bfba4194763
manuelj555 <programador.manuel@gmail.com>
2015-01-29 18:22:37 -0430

    Usando extension loggable para doctrine
    La Idea es poder saber que cambios han ocurrido localmente con cada
    traduccion en el transcurso del desarrollo

---
+++ Controller/LogController.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * This file is part of the Manuel Aguirre Project.
+ *
+ * (c) Manuel Aguirre <programador.manuel@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ManuelAguirre\Bundle\TranslationBundle\Controller;
+
+use ManuelAguirre\Bundle\TranslationBundle\Entity\Translation;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * @ autor Manuel Aguirre <programador.manuel@gmail.com>
+ * @Route("/logs")
+ */
+class LogController extends Controller
+{
+    /**
+     * @Route("/list/{id}")
+     */
+    public function listAction(Translation $translation)
+    {
+        $logs = $this->getDoctrine()
+            ->getManager()
+            ->getRepository('GedmoLoggable:LogEntry')
+            ->getLogEntries($translation);
+
+        return $this->render('@ManuelTranslation/Log/list.html.twig', array(
+            'logs' => $logs,
+            'translation' => $translation,
+        ));
+    }
+}
