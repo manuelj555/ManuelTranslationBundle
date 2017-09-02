10d2f3508aac696c5995135edb6f13cab40df2b1
Manuel Aguirre <maguirre@optimeconsulting.com>
2017-09-02 14:17:07 -0500

    Se agrega un nuevo compiler pass.
    Este compiler se encarga de añadir los recursos de traducción
    de los catalogos de doctrine al traductor.

--- ManuelTranslationBundle.php
+++ ManuelTranslationBundle.php
@@ -3,8 +3,10 @@
 namespace ManuelAguirre\Bundle\TranslationBundle;
 
 use ManuelAguirre\Bundle\TranslationBundle\DependencyInjection\Compiler\AddTranslatorLoadersPass;
+use ManuelAguirre\Bundle\TranslationBundle\DependencyInjection\Compiler\AddTranslatorResourcesPass;
 use ManuelAguirre\Bundle\TranslationBundle\DependencyInjection\Compiler\ConfigureExtractorsPass;
 use ManuelAguirre\Bundle\TranslationBundle\DependencyInjection\Compiler\LoggingTranslatorPass;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -14,6 +16,7 @@ class ManuelTranslationBundle extends Bundle
     {
         $container->addCompilerPass(new AddTranslatorLoadersPass());
         $container->addCompilerPass(new ConfigureExtractorsPass());
+        $container->addCompilerPass(new AddTranslatorResourcesPass(), PassConfig::TYPE_OPTIMIZE);
 
 //        if ($container->getParameter('kernel.debug')) {
 //            $container->addCompilerPass(new LoggingTranslatorPass());
