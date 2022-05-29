fe9c26eed112f292a55f3d575f6586b747b08edf
Manuel Aguirre <maguirre@optimeconsulting.com>
2022-05-29 13:02:21 -0500

    Configurando webpack encore

---
+++ public/build/app.js
@@ -0,0 +1,18 @@
+(self["webpackChunkmanuel_translation_bundle"] = self["webpackChunkmanuel_translation_bundle"] || []).push([["app"],{
+
+/***/ "./assets/js/app.js":
+/*!**************************!*\
+  !*** ./assets/js/app.js ***!
+  \**************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./assets/js/app.js"));
+/******/ }
+]);
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const Encore = require('@symfony/webpack-encore');
+
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('build/')
+    .addEntry('app', './assets/js/app.js')
+    .addEntry('config', './assets/js/config/index.js')
+    .splitEntryChunks()
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .configureBabel((config) => {
+        config.plugins.push('@babel/plugin-proposal-class-properties');
+    })
+    // enables @babel/preset-env polyfills
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+    //.enableSassLoader()
+    .enableReactPreset()
+;
+
+module.exports = Encore.getWebpackConfig();
